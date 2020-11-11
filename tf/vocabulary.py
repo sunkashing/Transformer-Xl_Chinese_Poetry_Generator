815d962e0ec4f9f8247f3fea872c13662aab2fa3
Shengloz <zhangshenglong1996@gmail.com>
2020-11-11 16:19:55 -0500

    deleted unused code

--- tf/vocabulary.py
+++ tf/vocabulary.py
@@ -28,8 +28,8 @@ class Vocab(object):
         # for zhihu dataset
         # todo delete here when test other datasets
         # self.min_freq = 100
-        # self.add_symbol('<UNK>')
-        # self.unk_idx = self.get_idx('<UNK>')
+        self.add_symbol('<UNK>')
+        self.unk_idx = self.get_idx('<UNK>')
 
     def tokenize(self, line, add_eos=False, add_double_eos=False):
         line = line.strip()
